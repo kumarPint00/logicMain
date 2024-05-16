403a443ea1fcf78786660740f443025e099ac46c
kumarpint00 <106054394+kumarpint0@users.noreply.github.com>
2024-05-16 17:59:55 +0530

    update the username

--- adminUI/src/pages/login/index.tsx
+++ adminUI/src/pages/login/index.tsx
@@ -107,7 +107,7 @@ const schema = yup.object().shape({
 
 const defaultValues = {
   password: 'admin',
-  email: 'admin@materio.com'
+  email: 'admin@logicrentalcars.com'
 }
 
 interface FormData {
@@ -264,10 +264,10 @@ const LoginPage = () => {
             </Box>
             <Alert icon={false} sx={{ py: 3, mb: 6, ...bgClasses.primaryLight, '& .MuiAlert-message': { p: 0 } }}>
               <Typography variant='caption' sx={{ mb: 2, display: 'block', color: 'primary.main' }}>
-                Admin: <strong>admin@materio.com</strong> / Pass: <strong>admin</strong>
+                Admin: <strong>admin@logicrentalcars.com</strong> / Pass: <strong>admin</strong>
               </Typography>
               <Typography variant='caption' sx={{ display: 'block', color: 'primary.main' }}>
-                Client: <strong>client@materio.com</strong> / Pass: <strong>client</strong>
+                Client: <strong>client@logicrentalcars.com</strong> / Pass: <strong>client</strong>
               </Typography>
             </Alert>
             <form noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
@@ -284,7 +284,7 @@ const LoginPage = () => {
                       onBlur={onBlur}
                       onChange={onChange}
                       error={Boolean(errors.email)}
-                      placeholder='admin@materio.com'
+                      placeholder='admin@logicrentalcars.com'
                     />
                   )}
                 />
